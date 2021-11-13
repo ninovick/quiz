6d34fc6d58fe2e2e49925a3cb39be31b1f310292
Elena Tsai <elena02@email.unc.edu>
2021-11-13 01:14:56 -0500

    phototime

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app: Flask = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+@app.route('/quiz')
+def quiz():
+    return render_template("quiz.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
