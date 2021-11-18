80ff8ff03a57d2d1a2e6cfb0ac57936d746b1c39
Naomi Novick <ninovick@email.unc.edu>
2021-11-18 10:23:59 -0500

    Reset flavor_dict in results

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
 @app.route('/quizpg1', methods=["GET", "POST"])
 def quizpg1():
     if request.method == "POST":
-        global sweet, sour, spicy, salty  
+        global flavor_dict  
         flavor = (request.form["image-pick"])
         flavor_dict[flavor] += 1
         return render_template("quizpg2.html")
@@ -24,7 +24,7 @@ def quizpg1():
 @app.route('/quizpg2', methods=["GET", "POST"])
 def quizpg2():
     if request.method == "POST":
-        global sweet, sour, spicy, salty  
+        global flavor_dict 
         flavor = (request.form["image-pick"])
         flavor_dict[flavor] += 1
         # return render_template("quizpg3.html")
@@ -33,7 +33,7 @@ def quizpg2():
 @app.route('/quizpg3', methods=["GET", "POST"])
 def quizpg3():
     if request.method == "POST":
-        global sweet, sour, spicy, salty  
+        global flavor_dict  
         flavor = (request.form["image-pick"])
         flavor_dict[flavor] += 1
         # return render_template("quizpg4.html")
@@ -42,7 +42,7 @@ def quizpg3():
 @app.route('/quizpg4', methods=["GET", "POST"])
 def quizpg4():
     if request.method == "POST":
-        global sweet, sour, spicy, salty, fav_flavs 
+        global flavor_dict, fav_flavs 
         flavor = (request.form["image-pick"])
         flavor_dict[flavor] += 1
 
@@ -52,9 +52,9 @@ def quizpg4():
 
 @app.route('/results',  methods=["GET", "POST"])
 def results():
-    global fav_flavs
+    global fav_flavs, flavor_dict
+    flavor_dict = {"Sweet": 0, "Sour": 0, "Spicy": 0, "Salty": 0}
     return render_template("results.html", fav_flavs=fav_flavs)
 
 if __name__ == '__main__':
-    flavor_dict: dict[str, int] = {"Sweet": 0, "Sour": 0, "Spicy": 0, "Salty": 0}
     app.run(debug=True)
